298d70f1a3cbe1fd8080c55fbd744d18f015f4d5
Nao D <decima.nao@gmail.com>
2025-03-10 15:56:25 -0300

    add mongoDb functions

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post("/api/shorturl", (req,res) => {
   if(!isValidUrl(url)){
     return res.json( { "error": 'Invalid URL' } )
   }else {
-    const existingUrl = await findUrlInDatabase(url); // Use await here
+    const existingUrl = findUrlInDatabase(url); // Use await here
     if (existingUrl) {
       return res.json({ original_url: existingUrl.original_url, short_url: existingUrl._id }); // Return existing URL data
     } else {
