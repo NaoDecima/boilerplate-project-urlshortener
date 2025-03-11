648e786e415902e608e9bfa24281fde89427b9e2
Nao D <decima.nao@gmail.com>
2025-03-11 20:48:31 -0300

    clean code

--- index.js
+++ index.js
@@ -32,51 +32,40 @@ app.get('/api/hello', function(req, res) {
 // URl Shortener
 
 //create a new schema and model for Url's in order to be saved in MongoDb
-let schema = new mongoose.schema({url: {type: String, required: true}})
+let schema = new mongoose.Schema({original_url: {type: String, required: true}})
 let Url = mongoose.model("Url", schema)
 
 
-function isValidUrl(url){
-  try{
-    let parsedUrl = new URL(url);
-    return /^(http|https):\/\/www\./.test(parsedUrl.href);
-  }catch(error){
-    return false;
-  }
-}
+//Handle post request
 
-app.post("/api/shorturl", (req,res) => {
-  let { url } = req.body;
+get.post("/api/shorturl", (req, res) =>{
+  let urlBody = req.body.url;
 
-  if(!isValidUrl(url)){
-    return res.json( {"error":'Invalid URL'} )
-  }else{
-    for(let key in urlDatabase){
-      if(urlDatabase[key] === url){
-        return res.json({ original_url : url, short_url : key} )
-      }
+  const isValidUrl = (urlBody) => {
+    try{
+      let parsedUrl = new URL(urlBody);
+      return /^(http|https):\/\/www\./.test(parsedUrl.href);
+    }catch(error){
+      return false;
     }
+  } 
+  if(!isValidUrl){
+    res.json({ error: "Invalid URL" })
+  }else{
+    const urlInstance = new Url({original_url: urlBody})
+    urlInstance.save()
+      .then(data => res.json({original_url: data, short_url: data.id}))
+      .catch(err => console.error(err))
   }
-  const urlInstance = new Url({url: url})
-  urlInstance.save()
-    .then( data => ({original_url: data.url, short_url: data.id}))
-    .catch(err => console.error(err))
-
-  /*let shortUrl = Object.keys(urlDatabase).length + 1 // return  an array with the amount of keys in the urlDatabase object and + 1  
-  urlDatabase[shortUrl] = url // at this point the parsed url is valid and is assigned to a new index in the url Database Object.
-  console.log(`url: ${url}, shortUrl: ${shortUrl}`)*/
-  res.json({ original_url : data.url, short_url : data.id} )
 })
 
-app.get("/api/shorturl/:short_url", (req,res) => {
-  let shortUrl = req.params.short_url   //when a GET request is made the value of Short_url is used...
-  let originalUrl = urlDatabase[shortUrl]; // ...as an index in urlDatabase to find the associated originalUrl.
-  console.log(`originalUrl: `+ originalUrl)
-  if(originalUrl){
-    return res.redirect(originalUrl) //if the originalUrl is in urlDatabase the user is redirected to it.
-  }else{
-    return res.json( {error: "No short URL found"} )
-  }
+app.get("/api/shorturl/:short_url?", (req, res) => {
+  let id = req.body.id;
+  urlInstance.findById(`${id}`)
+    .then(data => !data ?
+                   res.json({"No URL found"}) 
+                  :res.redirect(data.url) )
+    .catch(err => console.error(err))
 })
 
 
